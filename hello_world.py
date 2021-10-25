59198a44ed5ccdb30e702835509b5159b9997da2
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-24 23:03:00 -0700

    add string format

--- hello_world.py
+++ hello_world.py
@@ -64,4 +64,15 @@ first_name = "Zen"
 last_name = "Coder"
 age = 27
 print(f"My name is {first_name} {last_name} and I am {age} years old.") # Literal string interpolation
+print()
 
+# /////////////////////////////////
+print("String Format")
+first_name = "Zen"
+last_name = "Coder"
+age = 27
+print("My name is {} {} and I am {} years old.".format(first_name, last_name, age))
+# output: My name is Zen Coder and I am 27 years old.
+print("My name is {} {} and I am {} years old.".format(age, first_name, last_name))
+# output: My name is 27 Zen and I am Coder years old.
+print()
